dba46395de9bb22a444aaff2ec801df785940ab2
Jackson Bull <jacksonbull1987@gmail.com>
2020-01-09 18:18:56 -0500

    changes

---
+++ week_1/Calculator_class/Calculator.py
@@ -0,0 +1,21 @@
+class Calculator():
+    
+    def __init__(self, data):
+        self.data = data
+        
+    def length(self):
+        print(len(self.data))
+
+    def mean(self):
+        print(sum(self.data)/len.data)
+
+    def mean(self):
+        print(sum(self.data)/self.length)
+        
+    
+    
+    def add_data(data):
+        print(self.append(data))
+        
+#     def remove_data(data):
+#         print(self.pop(data))
