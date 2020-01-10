777d83884f39dc5b89e49a09560bdb55786533dc
Jackson Bull <jacksonbull1987@gmail.com>
2020-01-10 13:52:13 -0500

    changes

--- week_1/Calculator_class/Calculator.py
+++ week_1/Calculator_class/Calculator.py
@@ -3,19 +3,45 @@ class Calculator():
     def __init__(self, data):
         self.data = data
         
+#     def reset(self):
+#         self.__init__(self.data)
+    @property
     def length(self):
-        print(len(self.data))
-
+        return len(self.data)
+    
+    @property
     def mean(self):
-        print(sum(self.data)/len.data)
+        return sum(self.data)/len(self.data)
 
-    def mean(self):
-        print(sum(self.data)/self.length)
+    @property
+    def median(self):
+        #if self.length is odd
+        self.data.sort()
+        if self.length % 2 == 1:
+            return self.data[int((self.length)/2)]
+            
         
+        else:
+            return (self.data[((self.length)//2)] + self.data[((self.length) // 2)-1])/2
+            
+    @property
+    def variance(self):
+        return sum(((x - self.mean)**2) for x in self.data) / (len(self.data))
+    
+    @property
+    def stand_dev(self):
+        return (sum(((x - self.mean)**2) for x in self.data) / (len(self.data)))**.5
+         
+        
+    def add_data(self, more_data):
+        if type(more_data) == list:
+            return self.data.extend(more_data)
+            self.reset()
+        else:
+            return self.data.append(more_data)
+            self.reset()
+        
+    def remove_data(self, item):
+        return self.data.remove(item)
     
     
-    def add_data(data):
-        print(self.append(data))
-        
-#     def remove_data(data):
-#         print(self.pop(data))
--- week_1/Calculator_class/descriptive.py
+++ week_1/Calculator_class/descriptive.py
@@ -8,5 +8,50 @@ Created on Wed Sep  4 14:25:52 2019
 import math
 
 
-class Calculator:
-    pass        
+class Calculator():
+    
+    def __init__(self, data):
+        self.data = data
+        
+#     def reset(self):
+#         self.__init__(self.data)
+    @property
+    def length(self):
+        return len(self.data)
+    
+    @property
+    def mean(self):
+        return sum(self.data)/len(self.data)
+
+    @property
+    def median(self):
+        #if self.length is odd
+        self.data.sort()
+        if self.length % 2 == 1:
+            return self.data[int((self.length)/2)]
+            
+        
+        else:
+            return (self.data[((self.length)//2)] + self.data[((self.length) // 2)-1])/2
+            
+    @property
+    def variance(self):
+        return sum(((x - self.mean)**2) for x in self.data) / (len(self.data))
+    
+    @property
+    def stand_dev(self):
+        return (sum(((x - self.mean)**2) for x in self.data) / (len(self.data)))**.5
+         
+        
+    def add_data(self, more_data):
+        if type(more_data) == list:
+            return self.data.extend(more_data)
+            self.reset()
+        else:
+            return self.data.append(more_data)
+            self.reset()
+        
+    def remove_data(self, item):
+        return self.data.remove(item)
+    
+    
